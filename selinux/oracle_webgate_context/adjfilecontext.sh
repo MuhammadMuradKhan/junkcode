dd9d8b39d69482d03c73ff5a3c5488e392ecd7d7
Elia Pinto <yersinia.spiros@gmail.com>
2014-05-30 14:03:46 +0200

    adjfilecontext.sh: add some boolean

--- selinux/oracle_webgate_context/adjfilecontext.sh
+++ selinux/oracle_webgate_context/adjfilecontext.sh
@@ -21,6 +21,12 @@
 /usr/sbin/semanage fcontext -a -t httpd_sys_script_exec_t "/opt/netpoint/webgate/access/oblix/lang/en-us/securid-cgi(/.*)?" >/dev/null 2>&1 || :
 # auth plugin
 /usr/sbin/semanage fcontext -a -t textrel_shlib_t "/etc/httpd/WLSPlugin11.*(/.*)?/lib/.*\.so"
+# boolean
+
+setsebool -P httpd_execmem 1
+
+setsebool -P httpd_can_network_connect 1
+
 
 
 
