462dee5131e2d91f2c5806288932259bc9c357bc
Elia Pinto <yersinia.spiros@gmail.com>
2014-05-30 12:01:41 +0200

    adjfilecontext.sh: fixup fc on RHEL6 selinux policy
    Signed-off-by: Elia Pinto <yersinia.spiros@gmail.com>

--- selinux/oracle_webgate_context/adjfilecontext.sh
+++ selinux/oracle_webgate_context/adjfilecontext.sh
@@ -2,11 +2,11 @@
 /usr/sbin/semanage fcontext -a -t httpd_tmp_t "/opt/netpoint/webgate/access(/.*)?" >/dev/null 2>&1 || :
 /usr/sbin/semanage fcontext -a -t bin_t "/opt/netpoint/webgate(/.*)?bin(/.*)?"  >/dev/null 2>&1 || :
 # Text Relocation
-/usr/sbin/semanage fcontext -a -t texrel_shlib_t "/opt/netpoint/webgate/access/_jvmWebGate(/.*)?/.*\.so" >/dev/null 2>&1 || :
+/usr/sbin/semanage fcontext -a -t textrel_shlib_t "/opt/netpoint/webgate/access/_jvmWebGate(/.*)?/.*\.so" >/dev/null 2>&1 || :
 # shared lib
-/usr/sbin/semanage fcontext -a -t shlib_t "/opt/netpoint/webgate/access/oblix/lib/.+\.so(\.[^/]*)*" >/dev/null 2>&1 || :
-/usr/sbin/semanage fcontext -a -t shlib_t "/opt/netpoint/opt/netpoint./webgate/access/oblix/apps/webgate/bin/.+\.so(\.[^/]*)*" >/dev/null 2>&1 || :
-/usr/sbin/semanage fcontext -a -t shlib_t "/opt/netpoint/webgate/access/oblix/apps/webgate/bin/.+\.so(\.[^/]*)*" >/dev/null 2>&1 || :
+/usr/sbin/semanage fcontext -a -t textrel_shlib_t "/opt/netpoint/webgate/access/oblix/lib/.+\.so(\.[^/]*)*" >/dev/null 2>&1 || :
+/usr/sbin/semanage fcontext -a -t textrel_shlib_t "/opt/netpoint/opt/netpoint./webgate/access/oblix/apps/webgate/bin/.+\.so(\.[^/]*)*" >/dev/null 2>&1 || :
+/usr/sbin/semanage fcontext -a -t textrel_shlib_t "/opt/netpoint/webgate/access/oblix/apps/webgate/bin/.+\.so(\.[^/]*)*" >/dev/null 2>&1 || :
 # log file
 /usr/sbin/semanage fcontext -a -t httpd_log_t "/opt/netpoint/webgate/access/oblix/logs(/.*)?" >/dev/null 2>&1 || :
 # lock file
@@ -16,7 +16,7 @@
 # lock file
 /usr/sbin/semanage fcontext -a -t httpd_var_run_t "/opt/netpoint/webgate/access/oblix/config/.*\.lck" >/dev/null 2>&1 || :
 # cgi
-/usr/sbin/semanage fcontext -a -t httpd_script_exec_t "/opt/netpoint/webgate/access/oblix/lang/en-us/securid-cgi(/.*)?" >/dev/null 2>&1 || :
+/usr/sbin/semanage fcontext -a -t httpd_sys_script_exec_t "/opt/netpoint/webgate/access/oblix/lang/en-us/securid-cgi(/.*)?" >/dev/null 2>&1 || :
 
 
 /sbin/restorecon -Rv /opt/netpoint/webgate >/dev/null 2>&1 || :
