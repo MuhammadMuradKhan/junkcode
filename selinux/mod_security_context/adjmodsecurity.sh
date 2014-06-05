8a909a4fa7f88eb5544314ab2b23448d275e2b7b
Elia Pinto <yersinia.spiros@gmail.com>
2014-06-05 14:55:17 +0200

    adjmodsecurity.sh: add right security context for mlogc

--- selinux/mod_security_context/adjmodsecurity.sh
+++ selinux/mod_security_context/adjmodsecurity.sh
@@ -1,5 +1,7 @@
 # Fix wrong fs context for mod_security
 /usr/sbin/semanage fcontext -a -t httpd_var_lib_t "/var/lib/mod_security(/.*)?"
+#mlogc
+/usr/sbin/semanage fcontext -a -t httpd_log_t "/var/log/mlogc(/.*)?"
 
 /sbin/restorecon -Rv /var/lib/mod_security
 
