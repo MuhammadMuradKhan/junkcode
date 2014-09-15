f2f4931804edf20d4affa9b4171829e7a8b7debc
yersinia <yersinia.spiros@gmail.com>
2014-09-15 15:18:13 +0200

    adjfilecontext.sh: change httpd_log_t to httpd_cache_tadjfilecontext.sh

--- selinux/oracle_webgate_context/adjfilecontext.sh
+++ selinux/oracle_webgate_context/adjfilecontext.sh
@@ -16,7 +16,7 @@
 /usr/sbin/semanage fcontext -a -t textrel_shlib_t "/opt/netpoint/opt/netpoint./webgate/access/oblix/apps/webgate/bin/.+\.so(\.[^/]*)*"
 /usr/sbin/semanage fcontext -a -t textrel_shlib_t "/opt/netpoint/webgate/access/oblix/apps/webgate/bin/.+\.so(\.[^/]*)*"
 # log file
-/usr/sbin/semanage fcontext -a -t httpd_log_t "/opt/netpoint/webgate/access/oblix/logs(/.*)?"
+/usr/sbin/semanage fcontext -a -t httpd_cache_t "/opt/netpoint/webgate/access/oblix/logs(/.*)?"
 # lock file
 /usr/sbin/semanage fcontext -a -t httpd_var_run_t "/opt/netpoint/webgate(/.*)?/.*\.lck"
 # conf file
